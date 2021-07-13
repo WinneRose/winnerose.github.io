c24d2552d267cedae99dda9b1c3ad8150a361447
5elenay <5elenay@protonmail.com>
2021-07-13 14:55:51 +0300

    refactor: added prettierrc.

--- static/js/github.js
+++ static/js/github.js
@@ -1,64 +1,64 @@
 const repos = document.getElementById("repos");
 
 async function getRepo(user) {
-  const result = await fetch(`https://api.github.com/users/${user}/repos`);
-  let repos = await result.json();
+    const result = await fetch(`https://api.github.com/users/${user}/repos`);
+    let repos = await result.json();
 
-  repos = filterRepos(repos);
-  return repos;
+    repos = filterRepos(repos);
+    return repos;
 }
 
 function filterRepos(repos) {
-  const foundRepos = []
+    const foundRepos = [];
 
-  for (repo of repos) {
-    if (repo.fork) continue
+    for (repo of repos) {
+        if (repo.fork) continue;
 
-    repo.created_at = new Date(repo.created_at)
-    foundRepos.push(repo)
-  }
+        repo.created_at = new Date(repo.created_at);
+        foundRepos.push(repo);
+    }
 
-  const sortRepo = foundRepos.sort(
-    (a, b) => b.created_at - a.created_at
-  );
+    const sortRepo = foundRepos.sort((a, b) => b.created_at - a.created_at);
 
-  return sortRepo.slice(0, 12);
+    return sortRepo.slice(0, 12);
 }
 
 async function addRepos() {
-  for (repo of await getRepo("5elenay")) {
-    const repoDiv = document.createElement("div");
-    repoDiv.className = "repo";
+    for (repo of await getRepo("5elenay")) {
+        const repoDiv = document.createElement("div");
+        repoDiv.className = "repo";
 
-    const repoTitle = document.createElement("div");
-    repoTitle.className = "repo-title";
+        const repoTitle = document.createElement("div");
+        repoTitle.className = "repo-title";
 
-    const repoUrl = document.createElement("a");
-    repoUrl.innerText =
-      repo.name.length > 11 ? `${repo.name.substring(0, 8)}...` : repo.name;
-    repoUrl.href = repo.html_url;
-    repoUrl.target = "_blank";
+        const repoUrl = document.createElement("a");
+        repoUrl.innerText =
+            repo.name.length > 11
+                ? `${repo.name.substring(0, 8)}...`
+                : repo.name;
+        repoUrl.href = repo.html_url;
+        repoUrl.target = "_blank";
 
-    const tempDiv = document.createElement("div");
+        const tempDiv = document.createElement("div");
 
-    const starCount = document.createElement("p");
-    starCount.innerText = repo.stargazers_count;
+        const starCount = document.createElement("p");
+        starCount.innerText = repo.stargazers_count;
 
-    const imgFile = document.createElement("img");
-    imgFile.src = "./static/images/star.svg";
+        const imgFile = document.createElement("img");
+        imgFile.src = "./static/images/star.svg";
 
-    tempDiv.append(starCount, imgFile);
-    repoTitle.append(repoUrl, tempDiv);
+        tempDiv.append(starCount, imgFile);
+        repoTitle.append(repoUrl, tempDiv);
 
-    const repoDescription = document.createElement("p");
-    repoDescription.innerText =
-      repo.description.length > 76
-        ? `${repo.description.substring(0, 73)}...`
-        : repo.description;
+        const repoDescription = document.createElement("p");
+        repoDescription.innerText =
+            repo.description.length > 76
+                ? `${repo.description.substring(0, 73)}...`
+                : repo.description;
 
-    repoDiv.append(repoTitle, repoDescription);
-    repos.appendChild(repoDiv);
-  }
+        repoDiv.append(repoTitle, repoDescription);
+        repos.appendChild(repoDiv);
+    }
 }
 
 addRepos();
